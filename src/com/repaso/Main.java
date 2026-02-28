9e0b896a5d9a064ae13295036fd5c0a3c95ea6b3
Felipesandx <jsandovalr9@miumg.edu.gt>
2026-02-28 10:33:37 -0600

    Inicializando el proyecto

---
+++ src/com/repaso/Main.java
@@ -0,0 +1,10 @@
+package com.repaso;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module repasoProgra1 {
+}
