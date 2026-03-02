babc3779d34abd70ac040e1ac0704e1b13adad6a
Felipesandx <jsandovalr9@miumg.edu.gt>
2026-03-02 17:46:34 -0600

    Primer Ejercicio Terminado

--- src/com/repaso/Main.java
+++ src/com/repaso/Main.java
@@ -1,13 +1,30 @@
 package com.repaso;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	static Scanner sc = new Scanner (System.in);
 	public static void main(String[] args) {
 		
-
+		SumatoriaNumeroDivisible ();
 	}
-	public static void prueba () {
-		System.out.println("Hola mundo!");
+	//problema 1
+	public static void SumatoriaNumeroDivisible () {
+	
+		int n = 0;
+		int numero = 0;
+		int suma = 0;
+		final int DIVISOR = 4;
+		
+		System.out.println("Ingrese un número");
+		numero = sc.nextInt();
+		
+		while (n >= numero ) {
+			if(numero % DIVISOR ==0) {
+				suma = suma + numero;
+			}
+			numero++;
 		}
-
+		System.out.println("Respuesta: " + suma);
+	}
 }
